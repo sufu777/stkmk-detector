b038fb8d4a6db0d86f7306d6748bf9ee8a9aad52
sufu <528179780@qq.com>
2026-06-24 20:49:44 +0800

    添加github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
-        intellijIdea("2024.2")
+        intellijIdea("2023.1")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
         // Add plugin dependencies for compilation here:
